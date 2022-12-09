e14f6bef800188d7543150ead3fdf56d1482bfd6
Tongxi Lou <tongxi.lou@gmail.com>
2022-12-09 19:16:37 +0100

    refine the comments

--- _edit_config.sh
+++ _edit_config.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+
+# create new config
 python ./edit_config.py \
     --train_dir /app/project/data/train/ \
     --eval_dir /app/project/data/val/ \
@@ -6,5 +8,6 @@ python ./edit_config.py \
     --checkpoint /app/project/experiments/pretrained_model/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8/checkpoint/ckpt-0 \
     --label_map /app/project/experiments/label_map.pbtxt
 
+# move the new config files
 mkdir -p experiments/reference/
 mv pipeline_new.config experiments/reference/
--- data/_split.py
+++ data/_split.py
@@ -4,10 +4,17 @@ import random
 import shutil
 
 base = os.path.dirname(os.path.realpath(__file__))
-
 dir_in = os.path.join(base, 'processed')
 
 def move(fns: list, target: str, num: int) -> list:
+    '''Copy some files to the target directory, then return the name of the rest.
+    Params:
+        fns: the candidate files
+        target: the output path
+        num: number of files to copy; if it is -1 or larger than the length of fns, all files will be copied
+    Return:
+        List of the uncopied files. If none left, the return value is None.
+    '''
     if not os.path.isdir(target):
         os.mkdir(target)
     if num < 0 or num > len(fns):
@@ -20,11 +27,14 @@ def move(fns: list, target: str, num: int) -> list:
         shutil.copy(os.path.join(dir_in, fn), os.path.join(target, fn))
     return ret
 
+# prepare
 fns = os.listdir(dir_in)
 random.shuffle(fns)
 
+# copy
 fns = move(fns, os.path.join(base, 'test'), 10)
 fns = move(fns, os.path.join(base, 'train'), int(0.7 * len(fns)))
 fns = move(fns, os.path.join(base, 'val'), -1)
 
+# assure all files are used
 assert fns is None
